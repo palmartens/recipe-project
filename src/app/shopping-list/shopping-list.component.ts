b14244df62ecff2da0c1da9cad64daf93079c838
palmartens <Palmartens@ziggo.nl>
2022-12-01 11:54:19 +0100

    section 3 done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model'; 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Macaroni',500, 'grams'),
+    new Ingredient('Cheese', 250,'grams')  
+  ];
+
+
 
 }
