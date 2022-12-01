54a7bf0deadcfd5e7ce91a71ca7c4a05dcbcce6f
palmartens <Palmartens@ziggo.nl>
2022-12-01 16:39:17 +0100

    local References

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,13 +6,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent {
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement) {
+    
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
     // this.serverElements.push({
     //   type: 'server',
     //   name: this.newServerName,
@@ -20,8 +21,8 @@ export class CockpitComponent {
     // });
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   
     // this.serverElements.push({
     //   type: 'blueprint',
