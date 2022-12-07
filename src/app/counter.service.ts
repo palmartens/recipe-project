5db9af7a2c13ec431b0bba4dbc9b47edbeed01ea
palmartens <Palmartens@ziggo.nl>
2022-12-07 09:43:57 +0100

    services assignment

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -4,6 +4,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CounterService {
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
+
+  incrementActiveToInactiveCounter() {
+    this.activeToInactiveCounter++;
+    console.log(this.activeToInactiveCounter);
+  }
+
+  incrementInactiveToActiveCounter() {
+    this.inactiveToActiveCounter++;
+    console.log(this.inactiveToActiveCounter);
+  }
 
   constructor() { }
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,20 @@ export class UsersService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
+  constructor(private counterService: CounterService) { }
+
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActiveCounter();
+
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactiveCounter();
   }
 
-  constructor() { }
+  
 }
