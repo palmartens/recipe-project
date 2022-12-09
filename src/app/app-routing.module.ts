723e977b084e79beeb3a62c58de23d917b1c2cb7
palmartens <Palmartens@ziggo.nl>
2022-12-09 15:54:36 +0100

    activation guards + fake auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,10 @@ const appRoutes: Routes  = [
         { path: ':id/:name', component: UserComponent},  
       ] 
     },
-    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, 
+    { path: 'servers', 
+      //canActivate: [AuthGuard], 
+      canActivateChild: [AuthGuard], 
+      component: ServersComponent, 
       children: [
         { path: ':id', component: ServerComponent },
         { path: ':id/edit', component: EditServerComponent }
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
     constructor(private authService: AuthService, private router: Router) {}
 
@@ -16,6 +16,11 @@ export class AuthGuard implements CanActivate {
                 this.router.navigate(['/']);                
             }
         });        
+    }
+
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(childRoute, state);
+        
     }
 
 }
