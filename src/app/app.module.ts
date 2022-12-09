7eaf0b8fee094de1084a9276e76c91acbb7b54f9
palmartens <Palmartens@ziggo.nl>
2022-12-09 09:49:50 +0100

    routing (incl. programmatically)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users/:id/:name', component: UserComponent},
   { path: 'users', component: UsersComponent },
-  { path: 'servers', component: ServersComponent }
+  { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent }
 ];
 
 @NgModule({
