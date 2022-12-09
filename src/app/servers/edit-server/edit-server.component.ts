78adbd811527c573435cd8507b4710b517e9c7b3
palmartens <Palmartens@ziggo.nl>
2022-12-09 10:59:02 +0100

    routing

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,18 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // Snapshot: only fired once when de component has been loaded
+    console.log(`Snapshot queryparams:`,this.route.snapshot.queryParams);
+    console.log(`Snapshot fragment: ${this.route.snapshot.fragment}`);
+
+    // subscribe to route properties, triggers on every change
+    // no need to unsubscribe, angular does this for us
+    this.route.queryParams.subscribe((data) => { console.log('queryParams change',data) });
+    this.route.fragment.subscribe((data) => { console.log('fragment change',data) });
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
