269c516f30809351a35a7dd864e439be50f71a36
Philippe Martens <philippe.martens@cgi.com>
2023-03-01 09:32:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, Output } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
